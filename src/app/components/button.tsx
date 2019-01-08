632aad8d76858c205ec08368713421aa28cd9061
andres2906@gmail.com <andres2906@gmail.com>
2019-01-08 13:33:01 -0500

    Add click event on button component

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { DefaultProps } from '../common/default.props';
 
 export class ButtonComponent extends React.Component<DefaultProps,{}> {
+
+
+  handleClick(e: { preventDefault: () => void; }) {
+    e.preventDefault();
+    console.log('The button was clicked.');
+  }
+
+
   render() {
     return (
       <div>
-        <button>Enviar</button>
+        <button onClick={this.handleClick}>Enviar</button>
       </div>
     );
   }
