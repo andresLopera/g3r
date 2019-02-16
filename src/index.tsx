f8c1662f7fedf37e2d879dfd09761b216796c818
andres2906@gmail.com <andres2906@gmail.com>
2019-02-16 10:20:54 -0500

    Add link for manage style.scss

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppRouter } from './app/router';
 import * as serviceWorker from './serviceWorker';
+import './styles/index.scss';
 
 ReactDOM.render(
     <AppRouter />
